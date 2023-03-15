437b262d75c6fbe6812cee26694efdb2975147ad
thabozee <thabozee@gmail.com>
2023-03-15 19:20:24 +0200

    feat: removed imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,5 @@ import Head from "next/head";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  
-  return (
-    <>
-      <Script
-        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-        type="module"
-      />
-      <Script
-        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-        noModule
-      />
-      
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
